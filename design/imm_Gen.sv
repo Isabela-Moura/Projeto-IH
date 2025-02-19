537200d50731a460e13befc29ecb60d56f239d0c
Karina Lima <155447732+karinalimaklo@users.noreply.github.com>
2025-02-19 06:36:11 -0300

    Update imm_Gen.sv
    Adicionando JAL e JALR no Imm_gen

--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -30,6 +30,19 @@ module imm_Gen (
         inst_code[11:8],
         1'b0
       };
+	    
+	7'b1101111: /*J-type*/ // JAL
+      Imm_out = {
+	      inst_code[31] ? 11'h7FF : 11'b0,
+	      inst_code[31],
+	      inst_code[19:12],
+	      inst_code[20],
+	      inst_code[30:21],
+	      1'b0
+	    };
+	    
+	7'1100111: // JALR
+	    Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
       
    
 
