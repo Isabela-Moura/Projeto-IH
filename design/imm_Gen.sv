7ac5ee98a9205ff19b6ad197eeab8d07f2fea543
Karina Lima <155447732+karinalimaklo@users.noreply.github.com>
2025-02-19 13:51:44 -0300

    Atualizando I-type no Imm_gen.sv

--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -9,14 +9,20 @@ module imm_Gen (
 //Ele vai comparar o bit mais significativo (no caso das imediatas), para saber se é negativo ou positivo.
 // Se for 1 (neg), vai concatenar 1111..., caso contrario (pos) vai concatenar 00...
 // Tem que fazer isso pro ADDI, JAL, JALR
-
   always_comb
     case (inst_code[6:0])
       7'b0000011:  /*I-type load part*/
       Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
 			
-			7'b0010011:  /*I-type*/ // ADDI
-      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+      7'b0010011:  /*I-type*/ // Imediatas
+      	case(inst_code[14:12])
+      	3'b001: /*SLLI*/
+	      Imm_out = inst_cpde[24:20];
+      	3'b101: /*SRLI, SRAI*/
+	      Imm_out = inst_code[24:20];
+      	default: /*ADDI, SLTI*/
+	      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+      	endcase
       
       7'b0100011:  /*S-type*/
       Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:25], inst_code[11:7]};
@@ -30,8 +36,8 @@ module imm_Gen (
         inst_code[11:8],
         1'b0
       };
-	    
-	7'b1101111: /*J-type*/ // JAL
+      
+      7'b1101111: /*J-type*/ // JAL
       Imm_out = {
 	      inst_code[31] ? 11'h7FF : 11'b0,
 	      inst_code[31],
@@ -41,9 +47,8 @@ module imm_Gen (
 	      1'b0
 	    };
 	    
-	7'b1100111: // JALR
-	    Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
-      
+      7'b1100111: // JALR
+      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
    
 
       default: Imm_out = {32'b0};
