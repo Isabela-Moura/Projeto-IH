961e0dd35ecc7a717cc9428b104c3fd1b10522e9
Kauanny Barros <151306568+kauannybarros@users.noreply.github.com>
2025-02-19 08:48:55 -0300

    Ajuste

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -37,7 +37,7 @@ module ALUController (
 		  ((ALUOp == 2'b11) && (Funct3 == 3'b000))); // U-Jarl
 		  
   assign Operation[3] = (((ALUOp == 2'b01) && (Funct3 == 3'b000)) ||  // Branch-Beq
-      ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000))) || // R/I-Sub
+      ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)) || // R/I-Sub
 		  ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) || // R/I-Srai
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) || // R/I-Srli
 		  ((ALUOp == 2'b10) && (Funct3 == 3'b001) && (Funct7 == 7'b0000000)) || // R/I-Slli
